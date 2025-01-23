f73d4d43fbe979b4f13e5f68a52e01260249ee5e
bofoto <cho03k@gmail.com>
2025-01-23 15:13:14 +0900

    modified:   mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -22,7 +22,8 @@ document.addEventListener("DOMContentLoaded", function(){
   var modalBtn = document.getElementById("modalBtn");
   var modal = document.getElementById("myModal");
   var closeBtn = document.getElementById("closeBtn");
-
+  var cards = document.querySelectorAll(".card");
+  var modalContent = document.querySelector(".modal-content p");
   //functions
   function toggleModal(){
     modal.classList.toggle("show");
@@ -30,6 +31,25 @@ document.addEventListener("DOMContentLoaded", function(){
   //events
   modalBtn.addEventListener("click", toggleModal);
   closeBtn.addEventListener("click",toggleModal);
+  cards.forEach(function(card, index){
+    card.addEventListener("click", function(){
+      switch (index){
+        case 0:
+          modalContent.textContent = "Profill 카드에 들어갈 내용";
+          break;
+        case 1:
+          modalContent.textContent = "FrontEnd 카드에 들어갈 내용";
+          break;
+        case 2:
+          modalContent.textContent = "BackEnd 카드에 들어갈 내용";
+          break;
+        default:
+          modalContent.textContent = "정해지지않은 카드입니다."
+          break;
+      }
+      toggleModal();
+    })
+  })
 
   window,addEventListener("click", function (event){
     // 모달의 검은색 배경을 클릭해도 닫히도록 하는 코드
